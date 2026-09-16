15c872b91689041d3ad97e75d92849617f0a7fcc
SchmidtyPowerLaw <schmidtdstephanie@gmail.com>
2026-09-16 11:09:21 +0000

    Point site contact email to hymerschmidt@hotmail.com

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,4 +1,4 @@
 export const SITE_NAME = "Orange Law";
-export const SITE_EMAIL = "schmidtdstephanie@gmail.com";
+export const SITE_EMAIL = "hymerschmidt@hotmail.com";
 export const GIOVANNI_X = "https://x.com/Giovann35084111";
 export const PAPER_URL = "https://doi.org/10.5281/zenodo.19387099";
